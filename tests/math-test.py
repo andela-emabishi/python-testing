f08c03626d82b7a8838a88091ba2515f9658f9aa
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-03-03 17:01:07 +0300

    [Chore] Add .hound.yml

--- tests/math-test.py
+++ tests/math-test.py
@@ -5,4 +5,5 @@ from src.math import Math
 class MathTest(unittest.TestCase):
     def test_addition(self):
         # Make test fail
-        self.assertEqual(Math.addition(3, 4), 8)
+        # Add incorrect indentation
+         self.assertEqual(Math.addition(3, 4), 8)
